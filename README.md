b95615efb953d635c71599ed3a2401b740605dac
Mateus Almeida <mateusalmeida071@icloud.com>
2020-08-17 13:16:20 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#O Sorvetão - Site
+
+Site estático criado para a sorveteria [OSorvetão](https://www.osorvetao.com)
+
+## Author
+
+Mateus Almeida
---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/* Hamburger Menu */
+
+$(document).ready(function () {
+  $('.first-button').on('click', function () {
+    $('.animated-icon1').toggleClass('open');
+  });
+});
